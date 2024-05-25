f925084f5c0d15047a76a70c2aca3be41286761e
Ethan Mook <mook.e@northeastern.edu>
2024-05-25 21:37:25 +0200

    Update bio

--- email.js
+++ email.js
@@ -1,3 +1,4 @@
+// super advanced encryption wow
 function rot13(a) {
   return a.replace(/[a-zA-Z]/g, function(c){
     return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) 
